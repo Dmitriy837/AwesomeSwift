2e86c15e888dbd68949caf7564863ab5594b0578
Dmitriy837 <agnostic.88@yandex.ua>
2017-09-26 10:33:25 +0300

    Update AwesomeSwiftTests.swift
    No one forbids me to declare variables in different ways

--- AwesomeSwiftTests/AwesomeSwiftTests.swift
+++ AwesomeSwiftTests/AwesomeSwiftTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class AwesomeSwiftTests: XCTestCase {
     
     func testExample() {
-        let independentExpertInSwift: ExpertInSwift = SwiftFanBoy()
+        let independentExpertInSwift = SwiftFanBoy()
         XCTAssertEqual(independentExpertInSwift.opinionOnSwift(), "Swift is aswesome, I love it! Protocol oriented programming is a fantastic idea! So refreshing it is to learn something absolutely new! Protocol extension is a powerful tool! Swift is type safe and yet so powerful!")
     }
 }
